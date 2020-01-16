ede03a664c83313c8f6a61fd0ef266203ee4e1ed
ypkoo <ypkoo93@gmail.com>
2020-01-16 20:00:36 +0900

    'ch3'

---
+++ users/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='will',
+            email='will@email.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username='will',
+            email='will@email.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
